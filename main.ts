ecce9861b3f6f25215761f1a034bcb8ac516aa32
JacksonWengler <jwengler4304@stu.neisd.net>
2024-08-28 18:25:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,63 @@
+let dice = randint(1, 6)
+if (dice == 1) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+} else {
+    if (dice == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else {
+        if (dice == 3) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+        } else {
+            if (dice == 4) {
+                basic.showLeds(`
+                    # . . . #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    # . . . #
+                    `)
+            } else {
+                if (dice == 5) {
+                    basic.showLeds(`
+                        # . . . #
+                        . . . . .
+                        . . # . .
+                        . . . . .
+                        # . . . #
+                        `)
+                } else {
+                    if (dice == 6) {
+                        basic.showLeds(`
+                            . # . # .
+                            . . . . .
+                            . # . # .
+                            . . . . .
+                            . # . # .
+                            `)
+                    }
+                }
+            }
+        }
+    }
+}
+basic.forever(function () {
+	
+})
